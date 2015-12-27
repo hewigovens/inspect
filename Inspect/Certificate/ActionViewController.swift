c8555c59637e9ad1e35feddddcbbc2b5ba3993c4
hewigovens <hewigovens@gmail.com>
2015-12-28 02:36:26 +0800

    copy certs

--- Inspect/Certificate/ActionViewController.swift
+++ Inspect/Certificate/ActionViewController.swift
@@ -9,54 +9,87 @@
 import UIKit
 import MobileCoreServices
 
-class ActionViewController: UIViewController {
+class ActionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLSessionDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-
+    private lazy var tableView = UITableView(frame: CGRectZero, style: .Plain);
+    private var urlSession: NSURLSession?
+    private var certificates: [SecCertificate] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Get the item[s] we're handling from the extension context.
         
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
         for item: AnyObject in self.extensionContext!.inputItems {
             let inputItem = item as! NSExtensionItem
             for provider: AnyObject in inputItem.attachments! {
                 let itemProvider = provider as! NSItemProvider
-                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    itemProvider.loadItemForTypeIdentifier(kUTTypeImage as String, options: nil, completionHandler: { (image, error) in
-                        NSOperationQueue.mainQueue().addOperationWithBlock {
-                            if let strongImageView = weakImageView {
-                                strongImageView.image = image as? UIImage
+                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+                    itemProvider.loadItemForTypeIdentifier(kUTTypeURL as String, options: nil, completionHandler: { (item, error) -> Void in
+                        if let url = item as? NSURL?{
+                            print("get url \(url), scheme = \(url?.scheme)");
+                            if url?.scheme == ("https") {
+                                self.urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+                                let task = self.urlSession?.dataTaskWithURL(url!)
+                                if task != nil {
+                                    task!.resume();
+                                } else {
+                                    self.showError();
+                                }
                             }
                         }
-                    })
-                    
-                    imageFound = true
-                    break
+                    });
                 }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
             }
         }
     }
 
+    // MARK: Action
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
         self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
     }
-
+    
+    // MARK: UITableViewDelegate
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return certificates.count
+    }
+    
+    
+    // MARK: NSURLSessionDelegate
+    
+    func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+        
+        self.certificates = self.certificateDataForTrust(challenge.protectionSpace.serverTrust!)
+        self.configTableView()
+        completionHandler(.UseCredential, challenge.proposedCredential);
+    }
+    
+    
+    private func configTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    private func showError() {
+        
+    }
+    
+    private func certificateDataForTrust(trust: SecTrust) -> [SecCertificate] {
+        var certs: [SecCertificate] = []
+        for index in 0..<SecTrustGetCertificateCount(trust) {
+            if let cert = SecTrustGetCertificateAtIndex(trust, index) {
+                certs.append(cert)
+            }
+        }
+        return certs;
+    }
 }
