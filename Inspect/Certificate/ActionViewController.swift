56146d86acb720cf3290f958510d9e6f81ae1754
hewigovens <hewigovens@gmail.com>
2015-12-30 00:42:46 +0800

    query WOT

--- Inspect/Certificate/ActionViewController.swift
+++ Inspect/Certificate/ActionViewController.swift
@@ -10,87 +10,104 @@ import UIKit
 import MobileCoreServices
 
 class ActionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLSessionDelegate {
-
-    @IBOutlet weak var imageView: UIImageView!
-    private lazy var tableView = UITableView(frame: CGRectZero, style: .Plain);
-    private var urlSession: NSURLSession?
-    private var certificates: [SecCertificate] = []
+  
+  @IBOutlet weak var imageView: UIImageView!
+  private lazy var tableView = UITableView(frame: CGRectZero, style: .Plain);
+  private var urlSession: NSURLSession?
+  private var certificates: [SecCertificate] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.title = "Inspect - Certificate"
-        for item: AnyObject in self.extensionContext!.inputItems {
-            let inputItem = item as! NSExtensionItem
-            for provider: AnyObject in inputItem.attachments! {
-                let itemProvider = provider as! NSItemProvider
-                if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                    itemProvider.loadItemForTypeIdentifier(kUTTypeURL as String, options: nil, completionHandler: { (item, error) -> Void in
-                        if let url = item as? NSURL?{
-                            print("get url \(url), scheme = \(url?.scheme)");
-                            if url?.scheme == ("https") {
-                                self.urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: self, delegateQueue: NSOperationQueue.mainQueue())
-                                let task = self.urlSession?.dataTaskWithURL(url!)
-                                if task != nil {
-                                    task!.resume();
-                                } else {
-                                    self.showError();
-                                }
-                            }
-                        }
-                    });
+    self.title = "Inspect - Certificate"
+    
+    for item: AnyObject in self.extensionContext!.inputItems {
+      let inputItem = item as! NSExtensionItem
+      for provider: AnyObject in inputItem.attachments! {
+        let itemProvider = provider as! NSItemProvider
+        if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
+          itemProvider.loadItemForTypeIdentifier(kUTTypeURL as String, options: nil, completionHandler: { (item, error) -> Void in
+            if let url = item as? NSURL?{
+              print("get url \(url), scheme = \(url?.scheme)");
+              if url?.scheme == ("https") {
+                self.urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(), delegate: self, delegateQueue: NSOperationQueue.mainQueue())
+                let task = self.urlSession?.dataTaskWithURL(url!)
+                if task != nil {
+                  task!.resume();
+                  if let host = url?.host {
+                    WOT.query(host) { result in
+                      print(result)
+                      switch result {
+                      case .Success(let record):
+                        self.showWOTRating(record)
+                      case .Failure(let error):
+                        print(error)
+                      }
+                    }
+                  }
                 }
+              } else {
+                print("not https url")
+                self.showError()
+              }
             }
+          });
         }
+      }
     }
-
-    // MARK: Action
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: Action
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func done() {
+    self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
+  }
+  
+  // MARK: UITableViewDelegate
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return certificates.count
+  }
+  
+  
+  // MARK: NSURLSessionDelegate
+  
+  func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
+    
+    self.certificates = self.certificateDataForTrust(challenge.protectionSpace.serverTrust!)
+    self.configTableView()
+    completionHandler(.UseCredential, challenge.proposedCredential);
+  }
+  
+  
+  private func configTableView() {
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+  }
+  
+  private func showWOTRating(record: Record) {
+    
+  }
+  
+  private func showError() {
+    
+  }
+  
+  private func certificateDataForTrust(trust: SecTrust) -> [SecCertificate] {
+    var certs: [SecCertificate] = []
+    for index in 0..<SecTrustGetCertificateCount(trust) {
+      if let cert = SecTrustGetCertificateAtIndex(trust, index) {
+        certs.append(cert)
+      }
     }
-    
-    @IBAction func done() {
-        self.extensionContext!.completeRequestReturningItems(self.extensionContext!.inputItems, completionHandler: nil)
-    }
-    
-    // MARK: UITableViewDelegate
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return certificates.count
-    }
-    
-    
-    // MARK: NSURLSessionDelegate
-    
-    func URLSession(session: NSURLSession, didReceiveChallenge challenge: NSURLAuthenticationChallenge, completionHandler: (NSURLSessionAuthChallengeDisposition, NSURLCredential?) -> Void) {
-        
-        self.certificates = self.certificateDataForTrust(challenge.protectionSpace.serverTrust!)
-        self.configTableView()
-        completionHandler(.UseCredential, challenge.proposedCredential);
-    }
-    
-    
-    private func configTableView() {
-        self.tableView.delegate = self
-        self.tableView.dataSource = self
-    }
-    
-    private func showError() {
-        
-    }
-    
-    private func certificateDataForTrust(trust: SecTrust) -> [SecCertificate] {
-        var certs: [SecCertificate] = []
-        for index in 0..<SecTrustGetCertificateCount(trust) {
-            if let cert = SecTrustGetCertificateAtIndex(trust, index) {
-                certs.append(cert)
-            }
-        }
-        return certs;
-    }
+    return certs;
+  }
 }
--- Inspect/WOT/WOT.swift
+++ Inspect/WOT/WOT.swift
@@ -7,31 +7,88 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+public struct Record {
+    public enum Reputation: String {
+        case Excellent
+        case Good
+        case Unsatisfactory
+        case Poor
+        case VeryPoor
+    }
+    
+    public enum Category {
+        case Negative
+        case Questionable
+        case Neutral
+        case Positive
+    }
+    
+    var target: String
+    var score: Int
+    var code: Int
+    var reputation: Reputation {
+        get {
+            if self.score >= 80 {
+                return .Excellent
+            } else if self.score >= 60 {
+                return .Good
+            } else if self.score >= 40 {
+                return .Unsatisfactory
+            } else if self.score >= 20 {
+                return .Poor
+            } else {
+                return .VeryPoor
+            }
+        }
+    }
+    var category: Category {
+        get {
+            if self.code >= 500 {
+                return .Positive
+            } else if self.code >= 300 {
+                return .Neutral
+            } else if self.code >= 200 {
+                return .Questionable
+            } else {
+                return .Negative
+            }
+        }
+    }
+}
 
 public enum QueryResult {
-    case Success
+    case Success(Record)
     case Failure(NSError)
-}
-
-public enum Reputation {
-    case Excellent
-    case Good
-    case Unsatisfactory
-    case Poor
-    case VeryPoor
-}
-
-public enum Category {
-    
 }
 
 public class WOT: NSObject {
     
-    static let key = "e0d9e530f85c4c851e2638b898d4219321c01455"
-    static let apiUrl = "http://api.mywot.com/0.4/public_link_json2"
-    static func query(host: String, completion:(QueryResult)) -> Void {
-        let queryUrl = String(format: "%s?host=%s/&key=%s", WOT.apiUrl, host, WOT.key)
-        let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: queryUrl)!)
-        task.resume()
+    static func query(host: String, completion:(QueryResult) -> Void) -> Void {
+        let key = "e0d9e530f85c4c851e2638b898d4219321c01455"
+        let apiUrl = "http://api.mywot.com/0.4/public_link_json2"
+        let parameters = [
+            "hosts": String(format: "%@/", host),
+            "key": key
+        ];
+        Alamofire.request(.GET, apiUrl, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .Success:
+              if let value = response.result.value {
+                let json = JSON(value)
+                // todo category check
+                let array = json[host]["0"].array;
+                if let score = array?.first?.int {
+                  let record = Record(target: host, score: score, code: 300)
+                  completion(QueryResult.Success(record))
+                }
+              }
+            case .Failure(let error):
+                completion(QueryResult.Failure(error))
+                print(error)
+            }
+        }
     }
 }
