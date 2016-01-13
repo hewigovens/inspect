3279b6f39e33f9e14715be3ab5e9d32546f10353
hewigovens <hewigovens@gmail.com>
2016-01-13 22:45:10 +0800

    add hockeyapp

--- Inspect/Inspect/AppDelegate.swift
+++ Inspect/Inspect/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import HockeySDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        BITHockeyManager.sharedHockeyManager().configureWithIdentifier("399c05d7126848fabac6f9e2341880f8")
+        BITHockeyManager.sharedHockeyManager().startManager()
+        BITHockeyManager.sharedHockeyManager().authenticator.authenticateInstallation()
+        
         return true
     }
 
