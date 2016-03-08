321e847aa2311cf4430a3425d9b2124b045e4b37
hewigovens <hewigovens@gmail.com>
2016-03-09 00:22:06 +0800

    add rx

--- Certificate/PurchaseHelper.swift
+++ Certificate/PurchaseHelper.swift
@@ -15,20 +15,41 @@ class PurchaseHelper: NSObject,
 
     static let sharedHelper = PurchaseHelper()
     static let donationId = "donation_1"
-    var products = Dictionary<String, [SKProduct]>()
+    var products = [SKProduct]()
 
     static func makeDonation() {
-
-        let request = SKProductsRequest(productIdentifiers: Set(arrayLiteral: PurchaseHelper.donationId))
+        let request = SKProductsRequest(productIdentifiers: Set([PurchaseHelper.donationId]))
         request.delegate = PurchaseHelper.sharedHelper
         request.start()
+        SKPaymentQueue.defaultQueue().addTransactionObserver(PurchaseHelper.sharedHelper)
     }
 
     internal func productsRequest(request: SKProductsRequest, didReceiveResponse response: SKProductsResponse) {
+        for product in response.products {
+            self.products.append(product)
+        }
+        if self.products.count > 0 {
+            let payment = SKPayment(product: self.products[0])
+            SKPaymentQueue.defaultQueue().addPayment(payment)
+        }
+    }
 
+    internal func request(request: SKRequest, didFailWithError error: NSError) {
+        print(error)
     }
 
     internal func paymentQueue(queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-
+        for transaction in transactions {
+            switch transaction.transactionState {
+            case SKPaymentTransactionState.Purchased:
+                print("Transaction completed successfully.")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+            case SKPaymentTransactionState.Failed:
+                print("Transaction Failed")
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction)
+            default:
+                print(transaction.transactionState.rawValue)
+            }
+        }
     }
 }
