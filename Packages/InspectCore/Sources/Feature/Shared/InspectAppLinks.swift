aea77cbf85024072c7ade9377d1112be079b7bd6
hewigovens <360470+hewigovens@users.noreply.github.com>
2026-04-03 23:11:04 +0900

    update about url

--- Packages/InspectCore/Sources/Feature/Shared/InspectAppLinks.swift
+++ Packages/InspectCore/Sources/Feature/Shared/InspectAppLinks.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public enum InspectAppLinks {
     public static let appStore = URL(string: "https://apps.apple.com/us/app/inspect-view-tls-certificate/id1074957486")!
-    public static let about = URL(string: "https://fourplexlabs.github.io/Inspect/")!
+    public static let about = URL(string: "https://inspect.hewig.dev")!
 }
